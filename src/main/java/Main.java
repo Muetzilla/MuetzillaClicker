96a22ec1c25ed93177b38aa321df2c583d221595
Muetzilla <54369574+Muetzilla@users.noreply.github.com>
2021-02-26 23:34:07 +0100

    extends Application but does not the work :(

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.time.Instant;
 
-public class Main extends JFrame implements ActionListener {
-
-
-    JProgressBar pbar;
-    JButton button;
-    JLabel label;
-    JLabel currentLevel;
-    JPanel head;
-    JPanel footer;
-    JPanel left;
-    JPanel right;
-    JPanel panelIceWolf;
-    JLabel levelCost;
-    JLabel pbarTitel;
-    JLabel currentDamage;
-    JButton levelUp;
-    JButton iceWolfButton;
-    JLabel iceWolfLabel;
-    JLabel iceWolfDamageLabel;
-    JLabel iceWolfLevel;
-    Icon buttonIcon;
+public class Main extends Application {
 
     int clicks = 0;
     int clicksForStage = 0;
@@ -51,98 +34,18 @@ public class Main extends JFrame implements ActionListener {
         heroesRun.start();
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
     private void initComponents() {
-        setSize(1000, 500);
-        setTitle("MuetzillaClicker");
-        GridLayout headerLayout = new GridLayout(0, 5);
-        GridLayout footerLayout = new GridLayout(2, 1);
-        GridLayout leftLayout = new GridLayout(0, 2);
-        GridLayout heroesLayout = new GridLayout(3, 1);
 
-
-        //Init
-        head = new JPanel();
-        footer = new JPanel();
-        left = new JPanel();
-        right = new JPanel();
-
-        //Header
-        levelCost = new JLabel("Level Kosten: " + clicksNeededForNextLevel);
-        pbarTitel = new JLabel("Vortschritt in der aktuellen Stage");
-        currentDamage = new JLabel("Aktueller Schaden:" + clickDamage);
-        label = new JLabel("Klicks: " + clicks);
-        currentLevel = new JLabel("Aktuelles Level: " + clickerLevel);
-        levelUp = new JButton("LEVEL UP Clicker");
-
-        //Center
-        buttonIcon = new ImageIcon("images/clicker.png");
-        button = new JButton(buttonIcon);
-        button.setMnemonic('O');
-
-        //Center - Left
-        //IceWolf
-        iceWolfButton = new JButton("IceWolf");
-        iceWolfLabel = new JLabel("Ice Wolf Level:" + iceWolfCurrentLevel);
-        iceWolfLevel = new JLabel("Kosten: " + clicksNeededForNextLevelIW);
-        iceWolfDamageLabel = new JLabel("IceWolf Schaden: 0");
-        panelIceWolf = new JPanel();
-
-        //Footer
-        pbar = new JProgressBar();
-        pbar.setMinimum(MIN_VALUE);
-        pbar.setMaximum(MAX_VALUE);
-        pbar.setStringPainted(true);
-
-        //Hinzufügen
-
-        add(head, BorderLayout.NORTH);
-        add(footer, BorderLayout.SOUTH);
-        add(left, BorderLayout.WEST);
-        // add(right, BorderLayout.EAST);
-
-        //Head
-        head.setLayout(headerLayout);
-        head.add(label);
-        head.add(currentDamage);
-        head.add(currentLevel);
-        head.add(levelCost);
-        head.add(levelUp);
-
-        //Main Center
-        add(button, BorderLayout.CENTER);
-
-        //Main Left
-        left.setLayout(leftLayout);
-        left.add(panelIceWolf);
-        panelIceWolf.setLayout(heroesLayout);
-        panelIceWolf.add(iceWolfLabel);
-        panelIceWolf.add(iceWolfLevel);
-        panelIceWolf.add(iceWolfDamageLabel);
-        left.add(iceWolfButton);
-
-        //Footer
-        footer.setLayout(footerLayout);
-        footer.add(pbarTitel);
-        footer.add(pbar);
-
-        //Action Listener
-        button.addActionListener(this);
-        levelUp.addActionListener(this);
-        iceWolfButton.addActionListener(this);
-
-
-        setVisible(true);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                unixTimeEnd = Instant.now().getEpochSecond();
-                System.exit(0);
-            }
-        });
     }
 
 
-    @Override
+
+   
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
             clicks += clickDamage;
