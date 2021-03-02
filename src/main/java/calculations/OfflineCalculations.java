8b75557d4b8cecebd452f25883b92ee3b4b0f810
Muetzilla <54369574+Muetzilla@users.noreply.github.com>
2021-03-02 18:00:42 +0100

    Update OfflineCalculations.java

--- src/main/java/calculations/OfflineCalculations.java
+++ src/main/java/calculations/OfflineCalculations.java
@@ -1,7 +1,17 @@
 package calculations;
 
 public class OfflineCalculations {
-    public OfflineCalculations() {
+    private long unixTimeStart;
+    private long unixTimeEnd;
+    private long timeOffline;
 
+    public OfflineCalculations(long unixTimeStart, long unixTimeEnd) {
+        this.unixTimeStart = unixTimeStart;
+        this.unixTimeEnd = unixTimeEnd;
+       calculateOfflineClicks();
+    }
+
+    private void calculateOfflineClicks() {
+        timeOffline = unixTimeStart - unixTimeEnd;
     }
 }
